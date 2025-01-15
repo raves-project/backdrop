b6ee44a886cb361dd351a5472495f83632ede1eb
barrett <onkoe@protonmail.com>
2025-01-15 14:36:10 -0600

    feat(db): automatic migrations via `sqlx`

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -10,7 +10,17 @@ pub const THUMBNAILS_TABLE: &str = "thumbnail";
 pub static DATABASE: LazyLock<Pool<Sqlite>> = LazyLock::new(|| {
     const RAVES_DB_FILE_NAME: &str = "raves.db";
 
-    sqlx::Pool::<Sqlite>::connect_lazy(constcat::concat!("sqlite://", RAVES_DB_FILE_NAME))
-        .inspect_err(|e| tracing::error!("Failed to connect to media info database. err: {e}"))
-        .expect("err connecting to db")
+    let pool =
+        sqlx::Pool::<Sqlite>::connect_lazy(constcat::concat!("sqlite://", RAVES_DB_FILE_NAME))
+            .inspect_err(|e| tracing::error!("Failed to connect to media info database. err: {e}"))
+            .expect("err connecting to db");
+
+    // we'll also run migrations here real quick
+    _ = futures::executor::block_on(sqlx::migrate!("./migrations").run(&pool)).inspect_err(|e| {
+        tracing::error!(
+            "Database connection succeeded, but migrating the database failed! err: {e}"
+        )
+    });
+
+    pool
 });
