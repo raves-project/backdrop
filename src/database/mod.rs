30ec8fb90c1a4f1eb8f90e45918bd40d5b17bb87
barrett <onkoe@protonmail.com>
2025-01-16 22:20:16 -0600

    feat(db): make crate users specify db location

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,20 +1,65 @@
 //! Helps to connect to the database.
 
-use std::sync::LazyLock;
+use std::{
+    str::FromStr,
+    sync::{LazyLock, OnceLock},
+};
 
-use sqlx::{query::Query, sqlite::SqliteArguments, Pool, Sqlite};
+use camino::Utf8PathBuf;
+use sqlx::{
+    query::Query,
+    sqlite::{SqliteArguments, SqliteConnectOptions},
+    Pool, Sqlite,
+};
 
 pub const INFO_TABLE: &str = "info";
 pub const THUMBNAILS_TABLE: &str = "thumbnail";
 
+/// A path to the folder containing the backend's database.
+///
+/// DO NOT set this to the database file - it will fail to initialize.
+pub static DB_FOLDER_PATH: OnceLock<Utf8PathBuf> = OnceLock::new();
+
+/// The database pool.
+///
+/// You MUST set the [`DB_FOLDER_PATH`] before attempting to access this.
+/// Otherwise, the backend will panic!
 pub static DATABASE: LazyLock<Pool<Sqlite>> = LazyLock::new(|| {
-    const RAVES_DB_FILE_NAME: &str = "raves.sqlite";
+    const RAVES_DB_FILE: &str = "raves.sqlite";
 
-    let pool =
-        sqlx::Pool::<Sqlite>::connect_lazy(constcat::concat!("sqlite://", RAVES_DB_FILE_NAME))
-            .inspect_err(|e| tracing::error!("Failed to connect to media info database. err: {e}"))
-            .expect("err connecting to db");
+    // try to get the folder path (hoping the user has set the OnceLock static)
+    let Some(raves_db_folder) = DB_FOLDER_PATH.get() else {
+        tracing::error!("Attempted to access the database before initializing the path!");
+        tracing::error!("Since we don't know where the database is, the backend will now panic.");
+        panic!("No database folder path given.");
+    };
 
+    // ensure the path exists
+    match raves_db_folder.try_exists() {
+        Ok(true) => (),
+        Ok(false) => {
+            tracing::error!("The given database folder does not exist!");
+            panic!("Database folder doesn't exist.");
+        }
+        Err(e) => {
+            tracing::error!("Failed to check if database folder exists! err: {e}");
+            tracing::warn!("This might be because of file permissions.");
+            panic!("Couldn't check if database folder exists. err: {e}");
+        }
+    }
+
+    let options =
+        SqliteConnectOptions::from_str(&format!("sqlite://{raves_db_folder}/{RAVES_DB_FILE}"))
+            .inspect_err(|e| {
+                tracing::error!(
+                    "Failed to parse database string. The provided path may be incorrect. err: {e}"
+                )
+            })
+            .expect("database opts str")
+            .create_if_missing(true);
+
+    // connect to the pool
+    let pool = sqlx::Pool::<Sqlite>::connect_lazy_with(options);
     // we'll also run migrations here real quick
     _ = futures::executor::block_on(sqlx::migrate!("./migrations").run(&pool)).inspect_err(|e| {
         tracing::error!(
