1d88c95f3f9bf2467453447c2fcf9f5499e9f179
barrett <onkoe@protonmail.com>
2025-01-17 14:09:59 -0600

    feat(hash): add `InsertIntoTable` trait impl

--- src/models/media/hash.rs
+++ src/models/media/hash.rs
@@ -1,5 +1,8 @@
 use uuid::Uuid;
 
+use crate::{
+    database::{InsertIntoTable, DATABASE},
+};
 /// A hash for a media file, stored in the [`HASHES_TABLE`].
 #[derive(Clone, Debug, PartialEq, PartialOrd, Hash, sqlx::FromRow)]
 pub struct MediaHash {
@@ -7,6 +10,19 @@ pub struct MediaHash {
     pub media_id: Uuid,
     /// The media file's hash.
     pub hash: Vec<u8>,
+}
+
+
+impl InsertIntoTable for MediaHash {
+    #[tracing::instrument]
+    fn make_insertion_query(&self) -> Query<'_, Sqlite, SqliteArguments<'_>> {
+        // NOTE: if changing `HASHES_TABLE`, also change this!
+        sqlx::query!(
+            "INSERT INTO hashes (media_id, hash) VALUES ($1, $2)",
+            self.media_id,
+            self.hash
+        )
+    }
 }
 
 /// Whether a media file's hash is up-to-date.
